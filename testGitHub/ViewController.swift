b250f180878eeb76508991637539627e23efb7a3
MunaTayeb <memooo.166@gmail.com>
2017-03-21 15:33:05 +0300

    req

--- testGitHub/ViewController.swift
+++ testGitHub/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var Muna = "HI"
+        Muna = "Added Tima"
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +25,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+   
 
 }
 
